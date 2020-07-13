3a78dbe90101db940c87060afa8ca95b3e64f41f
joseph-gansukh <g.sukhbat@gmail.com>
2020-07-12 21:56:26 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const fedexAPI = require('fedex')
+
+var fedex = new fedexAPI({
+    environment: 'sandbox', // or live
+    debug: true,
+    key: '9doDUqleJG8uX9Rx',
+    password: 'sneHJVXzhx5Kcg8LGNM61Zgex',
+    account_number: '510087780',
+    meter_number: '114087373',
+    imperial: true // set to false for metric
+});
+
+
+fedex.track({
+    SelectionDetails: {
+        PackageIdentifier: {
+            Type: 'TRACKING_NUMBER_OR_DOORTAG',
+            Value: '7620543744'
+        }
+    }
+}, function(err, res) {
+    if (err) {
+        return console.log(err);
+    }
+
+    console.log(res.CompletedTrackDetails[0].TrackDetails);
+});
