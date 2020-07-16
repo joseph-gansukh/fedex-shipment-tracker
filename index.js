7246f7639edaa8b1bec3a8c76f811d73c576e6f0
Joseph S Gansukh <52771685+joseph-gansukh@users.noreply.github.com>
2020-07-15 21:25:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -25,4 +25,5 @@ fedex.track({
 
     console.log(res.CompletedTrackDetails[0].TrackDetails[0].StatusDetail.Description);
     console.log(res.CompletedTrackDetails[0].TrackDetails[0]);
+    console.log(res.CompletedTrackDetails[0].TrackDetails[0].StatusDetail.Events);
 });
